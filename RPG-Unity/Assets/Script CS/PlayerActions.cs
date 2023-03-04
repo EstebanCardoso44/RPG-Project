b4ab9ee99898cde755fcf800c0e04dff199abe7f
EstebanCardoso44 <90623113+EstebanCardoso44@users.noreply.github.com>
2023-03-04 10:09:11 +0100

    movement(playerinput+animation) OK + jump(playerinput ok , need debug animation) + teleportation between scene done

---
+++ RPG-Unity/Assets/Script CS/PlayerActions.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
+
+public class PlayerActions : MonoBehaviour
+{
+  public Rigidbody2D rb;
+  public Transform groundCheck;
+  public LayerMask groundLayer;
+
+  public Animator animator;
+
+  private float horizontal;
+  private float speed = 2f;
+  private float jumpingPower = 4f;
+  private bool isFacingRight = true;
+
+  void Update()
+  {
+    animator.SetFloat("Speed", Math.Abs(horizontal)); // link between our variable horizontal and our paramameters for animations in animator 
+
+    if (!isFacingRight && horizontal > 0f)
+    {
+      Flip();
+    }
+    else if (isFacingRight && horizontal < 0f)
+    {
+      Flip();
+    }
+  }
+
+  private void FixedUpdate()
+  {
+    rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+  }
+
+  public void Jump(InputAction.CallbackContext context)
+  {
+    if (context.performed && IsGrounded())
+    {
+      rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+      animator.SetBool("IsJumping", false);
+    }
+
+    if (context.canceled && rb.velocity.y > 0f)
+    {
+      rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+      animator.SetBool("IsJumping", true);
+    }
+  }
+
+  private bool IsGrounded()
+  {
+    return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+  }
+
+  private void Flip()
+  {
+    isFacingRight = !isFacingRight;
+    Vector3 localScale = transform.localScale;
+    localScale.x *= -1f;
+    transform.localScale = localScale;
+  }
+
+  public void Move(InputAction.CallbackContext context)
+  {
+    horizontal = context.ReadValue<Vector2>().x;
+  }
+}
---
+++ RPG-Unity/Assets/Script CS/TeleportLevel.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TeleportLevel : MonoBehaviour
+{
+  public int sceneBuildIndex;
+  // Start is called before the first frame update
+
+  // Update is called once per frame
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    print("Vous entrer dans un donjon !");
+    if (other.tag == "Player")
+    {
+      // Player entered, so move level
+      print("Scène numéro " + sceneBuildIndex);
+      SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+    }
+  }
+}
