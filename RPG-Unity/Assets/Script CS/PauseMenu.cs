5555bef07070ec74bb2fffd0c6dfd87e50eceea7
EstebanCardoso44 <90623113+EstebanCardoso44@users.noreply.github.com>
2023-03-09 15:00:39 +0100

    fix cam/menu

--- RPG-Unity/Assets/Script CS/PauseMenu.cs
+++ RPG-Unity/Assets/Script CS/PauseMenu.cs
@@ -14,7 +14,7 @@ public class PauseMenu : MonoBehaviour
   }
 
   // Update is called once per frame
- 
+
   void Update()
   {
     if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.JoystickButton7)) //key pressed activate code
