9dace9d284760196bf3e2619c45233677ed0a44f
EstebanCardoso44 <90623113+EstebanCardoso44@users.noreply.github.com>
2023-04-03 13:03:18 +0200

    death anim

--- RPG-Unity/Assets/Script CS/PlayerHealth.cs
+++ RPG-Unity/Assets/Script CS/PlayerHealth.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
+  Animator animator;
   // Start is called before the first frame update
-  public float health = 0f;
+  public float health = 100f;
   public float maxHealth = 100f;
+  public int scene;
   void Start()
   {
+    animator = GetComponent<Animator>();
     health = maxHealth;
   }
   public void UpdateHealth(float mod)
@@ -27,6 +31,19 @@ public class PlayerHealth : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
+    if (health == 0f)
+    {
+      OnDie();
+    }
 
+  }
+  public void OnDie() // Die animation on trigger
+  {
+    animator.SetTrigger("Die");
+  }
+  public void ReSpawn()
+  {
+    int scene = SceneManager.GetActiveScene().buildIndex;
+    SceneManager.LoadScene(scene, LoadSceneMode.Single);
   }
 }
