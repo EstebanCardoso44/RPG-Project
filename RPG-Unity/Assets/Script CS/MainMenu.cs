ad284ad0f6cc36701a0c46996ee487938fdcadce
Yassine ENNOUHI <yassine.ennouhi@ynov.com>
2023-04-16 18:21:26 +0200

    fixes

--- RPG-Unity/Assets/Script CS/MainMenu.cs
+++ RPG-Unity/Assets/Script CS/MainMenu.cs
@@ -34,8 +34,17 @@ public class MainMenu : MonoBehaviour
   {
     SceneManager.LoadScene("Dungeon1");
   }
-  public void Quit() // quit game
-  {
+  public void Quit() {
+  #if (UNITY_EDITOR || DEVELOPMENT_BUILD)
+    Debug.Log(this.name+" : "+this.GetType()+" : "+System.Reflection.MethodBase.GetCurrentMethod().Name); 
+  #endif
+  #if (UNITY_EDITOR)
+    UnityEditor.EditorApplication.isPlaying = false;
+  #elif (UNITY_STANDALONE) 
     Application.Quit();
+  #elif (UNITY_WEBGL)
+    Application.OpenURL("https://itch.io/");
+  #endif
   }
+
 }
